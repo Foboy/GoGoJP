da39a73b3c418d9d7891f07fba84681aedc5db80
zhengrunqiang <653260669@qq.com>
2014-04-30 14:06:59 +0800

    1

---
+++ Application/Common/Common/DataResult.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Common\Common;
+
+class DataResult
+{
+	public $Error;
+
+	public $Id;
+
+	public $ExMessage;
+
+	public $ErrorMessage;
+
+	public $Data;
+
+	public function __construct(){
+		$this->Error = ErrorType::Success;
+	}
+}
---
+++ Application/Common/Common/ErrorType.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Common\Common;
+
+class ErrorType
+{
+	const Success = 0;
+	const Failed = 1;
+	const LoginFailed = 2;
+	const Unlogin = 11;
+	const RequestParamsFailed = 3;
+	const Accessdenied = 4;
+}
