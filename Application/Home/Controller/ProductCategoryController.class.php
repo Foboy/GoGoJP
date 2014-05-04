59e10923cfd0c0f99369ab8a27ae6b285fce9b98
zhengrunqiang <653260669@qq.com>
2014-05-04 23:34:47 +0800

    添加幻灯片，商品分类接口

--- Application/Home/Controller/ProductCategoryController.class.php
+++ Application/Home/Controller/ProductCategoryController.class.php
@@ -5,4 +5,20 @@ class ProductCategoryController extends Controller {
     public function index(){
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Home模块的ProductCategory控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
+    //添加商品类别 主类别 parentid=0
+    public  function addProductCategory(){
+    	
+    }
+    //删除分类
+    public function deleteProductCategory(){
+    	
+    }
+    //编辑类别
+    public function updateProductCategory(){
+    	
+    }
+    //获取分类列表信息
+    public function searchProductCategory(){
+    	
+    }
 }
