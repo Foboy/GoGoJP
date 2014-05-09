bcb47cab4243572b334abbdf4848eed762807230
zhengrunqiang <653260669@qq.com>
2014-05-09 16:50:31 +0800

    123

--- Application/Home/Model/MessageModel.class.php
+++ Application/Home/Model/MessageModel.class.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * @author yangchao
  * @email:66954011@qq.com
  * @date: 2014/5/9 11:53:28
  */
 namespace Home\Model;
+
 use Think\Model;
 use Common\Common\PageDataResult;
 use Think\Db\Driver\Pdo;
@@ -14,13 +16,13 @@ use Common\Common\ErrorType;
 class MessageModel extends Model {
 	protected $tableName = 'message';
 	// 增加表中数据
-	public function addModel($form_userid,$to_userid,$content,$create_time) {
+	public function addModel($form_userid, $to_userid, $content, $create_time) {
 		$result = new DataResult ();
 		$data = array (
-':form_userid' => $form_userid,
-                   ':to_userid' => $to_userid,
-                   ':content' => $content,
-                   ':create_time' => $create_time
+				':form_userid' => $form_userid,
+				':to_userid' => $to_userid,
+				':content' => $content,
+				':create_time' => $create_time 
 		);
 		$pid = $this->add ( $data );
 		if ($pid > 0) {
@@ -35,8 +37,8 @@ class MessageModel extends Model {
 	// 删除表中数据
 	public function deleteModel($messageid) {
 		$result = new DataResult ();
-        $map['messageid']=$messageid;
-		if ($this->where ($map)->delete () == 1) {
+		$map ['messageid'] = $messageid;
+		if ($this->where ( $map )->delete () == 1) {
 			$result->ErrorMessage = '删除成功';
 		} else {
 			$result->Error = ErrorType::Failed;
@@ -45,19 +47,19 @@ class MessageModel extends Model {
 		return $result;
 	}
 	// 编辑表中数据
-	public function updateModel($messageid,$form_userid,$to_userid,$content,$create_time,$advisory_id) {
+	public function updateModel($messageid, $form_userid, $to_userid, $content, $create_time, $advisory_id) {
 		$result = new DataResult ();
 		$data = array (
-':messageid' => $messageid,
-                   ':form_userid' => $form_userid,
-                   ':to_userid' => $to_userid,
-                   ':content' => $content,
-                   ':create_time' => $create_time,
-                   ':advisory_id' => $advisory_id
+				':messageid' => $messageid,
+				':form_userid' => $form_userid,
+				':to_userid' => $to_userid,
+				':content' => $content,
+				':create_time' => $create_time,
+				':advisory_id' => $advisory_id 
 		);
 		// 注意判断条件使用恒等式
-        $map['messageid']=$messageid;
-		if ($this->where ($map )->save ( $data ) !== false) {
+		$map ['messageid'] = $messageid;
+		if ($this->where ( $map )->save ( $data ) !== false) {
 			$result->Data = $this->find ( $messageid );
 			$result->ErrorMessage = '更新成功';
 		} else {
@@ -69,12 +71,12 @@ class MessageModel extends Model {
 	// 根据主键id获取某个专辑信息
 	public function getModel($messageid) {
 		$result = new DataResult ();
-        $map['messageid']=$messageid;
-		$result->Data = $this->where ($map )->select ();
+		$map ['messageid'] = $messageid;
+		$result->Data = $this->where ( $map )->select ();
 		return $result;
 	}
 	// 获取图片管理表中分页数据
-	public function searchByPage($form_userid,$to_userid,$content,$create_time,$advisory_id, $pageindex, $pagesize) {
+	public function searchByPage($form_userid, $to_userid, $content, $create_time, $advisory_id, $pageindex, $pagesize) {
 		$result = new PageDataResult ();
 		$lastpagenum = $pageindex * $pagesize;
 		$conn = new Pdo ();
@@ -84,25 +86,25 @@ class MessageModel extends Model {
  and  ( create_time = :create_time or :create_time='' )
  and  ( advisory_id = :advisory_id or :advisory_id=0 )
  limit $lastpagenum,$pagesize", array (
-':form_userid' => $form_userid,
-                   ':to_userid' => $to_userid,
-                   ':content' => $content,
-                   ':create_time' => $create_time,
-                   ':advisory_id' => $advisory_id
-			)  );
+				':form_userid' => $form_userid,
+				':to_userid' => $to_userid,
+				':content' => $content,
+				':create_time' => $create_time,
+				':advisory_id' => $advisory_id 
+		) );
 		$data = $conn->query ( " select count(*) totalcount  from gogojp_message where  ( form_userid = :form_userid or :form_userid=0 )
  and  ( to_userid = :to_userid or :to_userid=0 )
  and  ( content = :content or :content='' )
  and  ( create_time = :create_time or :create_time='' )
  and  ( advisory_id = :advisory_id or :advisory_id=0 )
 ", array (
-':form_userid' => $form_userid,
-                   ':to_userid' => $to_userid,
-                   ':content' => $content,
-                   ':create_time' => $create_time,
-                   ':advisory_id' => $advisory_id
-			)  );
-		$totalcount=$data[0]['totalcout'];
+				':form_userid' => $form_userid,
+				':to_userid' => $to_userid,
+				':content' => $content,
+				':create_time' => $create_time,
+				':advisory_id' => $advisory_id 
+		) );
+		$totalcount = $data [0] ['totalcout'];
 		$result->pageindex = $pageindex;
 		$result->pagesize = $pagesize;
 		$result->Data = $objects;
