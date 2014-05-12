27d56f98edc081d4b8aa5ddbbc76f63f1ab4ff16
kbtyc <kbtyc@163.com>
2014-05-13 00:10:22 +0800

    产品规格

---
+++ Application/Home/Controller/StandardController.class.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * @author yangchao
+ * @email:66954011@qq.com
+ * @date: 2014/5/13 0:08:46
+ */
+namespace Home\Controller;
+use Think\Controller;
+use Home\Model\StandardModel;
+use Common\Common\DataResult;
+use Common\Common\ErrorType;
+
+class StandardController extends Controller {
+	public function index() {
+		$this->show ( '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Home模块的Standard控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>', 'utf-8' );
+	}
+	// 添加
+	public function addStandard() {
+    $result =new DataResult();
+		$Standard = new StandardModel ();
+
+           $parent_name = I ( 'parent_name' );  
+$standard_parent_id = I ( 'standard_parent_id' );  
+$child_name = I ( 'child_name' );  
+$create_time = I ( 'create_time' );  
+
+          
+           if (! isset ( $parent_name ) or empty ( $parent_name )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+           $result->ErrorMessage="'parent_name' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $standard_parent_id ) or empty ( $standard_parent_id )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+           $result->ErrorMessage="'standard_parent_id' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $child_name ) or empty ( $child_name )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+           $result->ErrorMessage="'child_name' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $create_time ) or empty ( $create_time )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+           $result->ErrorMessage="'create_time' params error";
+			$this->ajaxReturn($result);
+		   }
+        $result= $Standard->addModel ( $parent_name,$standard_parent_id,$child_name,$create_time );
+		$this->ajaxReturn ( $result );
+	}
+	// 删除
+	public function deleteStandard() {
+     $result =new DataResult();
+    		$Standard = new StandardModel ();
+		$standard_id = I ( 'standard_id' );
+		           if (! isset ( $standard_id ) or empty ( $standard_id )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'standard_id' params error";
+			$this->ajaxReturn($result);
+		   }
+        $result=  $Standard->deleteModel ( $standard_id );
+		$this->ajaxReturn ( $result );
+	}
+	// 编辑
+	public function updateStandard() {
+    
+     $result =new DataResult();
+		$Standard = new StandardModel ();
+      $parent_name = I ( 'parent_name' );  
+$standard_parent_id = I ( 'standard_parent_id' );  
+$child_name = I ( 'child_name' );  
+$create_time = I ( 'create_time' );  
+
+          
+           if (! isset ( $parent_name ) or empty ( $parent_name )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'parent_name' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $standard_parent_id ) or empty ( $standard_parent_id )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'standard_parent_id' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $child_name ) or empty ( $child_name )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'child_name' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $create_time ) or empty ( $create_time )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'create_time' params error";
+			$this->ajaxReturn($result);
+		   }
+        $result= $Standard->updateModel (  $parent_name,$standard_parent_id,$child_name,$create_time );
+		$this->ajaxReturn ( $result );
+	}
+	//获取单个
+	public function getStandard(){
+      $result =new DataResult();
+		$Standard = new StandardModel ();
+		$standard_id=I('standard_id');
+		           if (! isset ( $standard_id ) or empty ( $standard_id )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'standard_id' params error";
+			$this->ajaxReturn($result);
+		   }
+                $result= $Standard->getModel($standard_id);
+		$this->ajaxReturn ( $result );
+	}
+
+	// 分页查询列表
+	public function searchStandard() {
+     $result =new DataResult();
+		$Standard = new StandardModel ();
+           $parent_name = I ( 'parent_name' );  
+$standard_parent_id = I ( 'standard_parent_id' );  
+$child_name = I ( 'child_name' );  
+$create_time = I ( 'create_time' );  
+
+           if (! isset ( $parent_name ) or empty ( $parent_name )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'parent_name' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $standard_parent_id ) or empty ( $standard_parent_id )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'standard_parent_id' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $child_name ) or empty ( $child_name )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'child_name' params error";
+			$this->ajaxReturn($result);
+		   }
+           if (! isset ( $create_time ) or empty ( $create_time )) {
+			$result->Error=ErrorType::RequestParamsFailed;
+                $result->ErrorMessage="'create_time' params error";
+			$this->ajaxReturn($result);
+		   }
+		$pageIndex = I ('pageindex', 0 );
+		$pageSize = I ('pagesize', 10 );
+                   $result= $Standard->searchByPage ( $parent_name,$standard_parent_id,$child_name,$create_time,$pageIndex, $pageSize ) ;
+		$this->ajaxReturn ( $result );
+	}
+}
---
+++ Application/Home/Model/StandardModel.class.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @author yangchao
+ * @email:66954011@qq.com
+ * @date: 2014/5/13 0:07:14
+ */
+namespace Home\Model;
+use Think\Model;
+use Common\Common\PageDataResult;
+use Think\Db\Driver\Pdo;
+use Common\Common\DataResult;
+use Common\Common\ErrorType;
+
+class StandardModel extends Model {
+	protected $tableName = 'standard';
+	// 增加表中数据
+	public function addModel($parent_name,$standard_parent_id,$child_name,$create_time) {
+		$result = new DataResult ();
+		$data = array (
+'parent_name' => $parent_name,
+                   'standard_parent_id' => $standard_parent_id,
+                   'child_name' => $child_name,
+                   'create_time' => $create_time
+		);
+		$pid = $this->add ( $data );
+		if ($pid > 0) {
+			$result->Data = $this->find ( $pid );
+			$result->ErrorMessage = '新增成功';
+		} else {
+			$result->Error = ErrorType::Failed;
+			$result->ErrorMessage = '新增失败';
+		}
+		return $result;
+	}
+	// 删除表中数据
+	public function deleteModel($standard_id) {
+		$result = new DataResult ();
+        $map['standard_id']=$standard_id;
+		if ($this->where ($map)->delete () == 1) {
+			$result->ErrorMessage = '删除成功';
+		} else {
+			$result->Error = ErrorType::Failed;
+			$result->ErrorMessage = '删除失败';
+		}
+		return $result;
+	}
+	// 编辑表中数据
+	public function updateModel($standard_id,$parent_name,$standard_parent_id,$child_name,$create_time) {
+		$result = new DataResult ();
+		$data = array (
+'standard_id' => $standard_id,
+                   'parent_name' => $parent_name,
+                   'standard_parent_id' => $standard_parent_id,
+                   'child_name' => $child_name,
+                   'create_time' => $create_time
+		);
+		// 注意判断条件使用恒等式
+        $map['standard_id']=$standard_id;
+		if ($this->where ($map )->save ( $data ) !== false) {
+			$result->Data = $this->find ( $standard_id );
+			$result->ErrorMessage = '更新成功';
+		} else {
+			$result->Error = ErrorType::Failed;
+			$result->ErrorMessage = '更新成功';
+		}
+		return $result;
+	}
+	// 根据主键id获取某个专辑信息
+	public function getModel($standard_id) {
+		$result = new DataResult ();
+        $map['standard_id']=$standard_id;
+		$result->Data = $this->where ($map )->find ();
+		return $result;
+	}
+	// 获取图片管理表中分页数据
+	public function searchByPage($parent_name,$standard_parent_id,$child_name,$create_time, $pageindex, $pagesize) {
+		$result = new PageDataResult ();
+		$lastpagenum = $pageindex * $pagesize;
+		$conn = new Pdo ();
+		$objects = $conn->query ( " select standard_id,parent_name,standard_parent_id,child_name,create_time from gogojp_standard where  ( parent_name = :parent_name or :parent_name='' ) 
+ and  ( standard_parent_id = :standard_parent_id or :standard_parent_id=0 ) 
+ and  ( child_name = :child_name or :child_name='' ) 
+ and  ( create_time = :create_time or :create_time='' ) 
+ limit $lastpagenum,$pagesize", array (
+':parent_name' => $parent_name,
+                   ':standard_parent_id' => $standard_parent_id,
+                   ':child_name' => $child_name,
+                   ':create_time' => $create_time
+			)  );
+		$data = $conn->query ( " select count(*) totalcount  from gogojp_standard where  ( parent_name = :parent_name or :parent_name='' ) 
+ and  ( standard_parent_id = :standard_parent_id or :standard_parent_id=0 ) 
+ and  ( child_name = :child_name or :child_name='' ) 
+ and  ( create_time = :create_time or :create_time='' ) 
+", array (
+':parent_name' => $parent_name,
+                   ':standard_parent_id' => $standard_parent_id,
+                   ':child_name' => $child_name,
+                   ':create_time' => $create_time
+			)  );
+		$totalcount=$data[0]['totalcount'];
+		$result->pageindex = $pageindex;
+		$result->pagesize = $pagesize;
+		$result->Data = $objects;
+		$result->totalcount = $totalcount;
+		return $result;
+	}
+}
